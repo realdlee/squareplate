1b355a652cf1b24509906717664039b8aab67745
David Lee <ambitiouspear@gmail.com>
2013-02-23 13:35:08 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squareplate::Application.config.secret_token = 'bad1979ad8c56088330d551f8ca1aa4329c488eba95bd6afd81066c2edebf3749a1047e30de9e85a70fa3997f752888268db78efd306368644a3a9c8efcee40f'
