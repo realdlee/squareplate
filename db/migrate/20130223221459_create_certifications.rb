8f9c08db1cfe23ba6f2c7cfe9a04fa76c3b34612
Jeff Magee <jmagee@twitter.com>
2013-02-23 14:56:13 -0800

    added default certification values

--- db/migrate/20130223221459_create_certifications.rb
+++ db/migrate/20130223221459_create_certifications.rb
@@ -9,86 +9,86 @@ class CreateCertifications < ActiveRecord::Migration
       t.boolean :mand3
       t.boolean :mand4
       t.boolean :mand5
-      t.integer :safety1_c
-      t.integer :safety1_d
-      t.boolean :nutrition1_c
-      t.integer :nutrition1_d
-      t.boolean :nutrition2_c
-      t.integer :nutrition2_d
-      t.boolean :nutrition3_c
-      t.integer :nutrition3_d
-      t.boolean :nutrition4_c
-      t.integer :nutrition4_d
-      t.boolean :nutrition5_c
-      t.integer :nutrition5_d
-      t.float :labor1_c
-      t.integer :labor1_d
-      t.float :labor2_c
-      t.integer :labor2_d
-      t.float :labor3_c
-      t.integer :labor3_d
-      t.float :labor4_c
-      t.integer :labor4_d
-      t.boolean :labor5_c
-      t.integer :labor5_d
-      t.boolean :labor6_c
-      t.integer :labor6_d
-      t.boolean :labor7_c
-      t.integer :labor7_d
-      t.boolean :community1_c
-      t.integer :community1_d
-      t.boolean :community2_c
-      t.integer :community2_d
-      t.boolean :community3_c
-      t.integer :community3_d
-      t.boolean :community4_c
-      t.integer :community4_d
-      t.float :source1_c
-      t.integer :source1_d
-      t.boolean :conserv1_c
-      t.integer :conserv1_d
-      t.boolean :conserv2_c
-      t.integer :conserv2_d
-      t.boolean :conserv3_c
-      t.integer :conserv3_d
-      t.boolean :conserv4_c
-      t.integer :conserv4_d
-      t.boolean :conserv5_c
-      t.integer :conserv5_d
-      t.boolean :conserv6_c
-      t.integer :conserv6_d
-      t.boolean :conserv7_c
-      t.integer :conserv7_d
-      t.boolean :conserv8_c
-      t.integer :conserv8_d
-      t.float :conserv9_c
-      t.integer :conserv9_d
-      t.boolean :conserv10_c
-      t.integer :conserv10_d
-      t.boolean :conserv11_c
-      t.integer :conserv11_d
-      t.boolean :conserv12_c
-      t.integer :conserv12_d
-      t.boolean :conserv13_c
-      t.integer :conserv13_d
-      t.boolean :conserv14_c
-      t.integer :conserv14_d
+      t.integer :safety1_c, :default => 0
+      t.integer :safety1_d, :default => 0
+      t.boolean :nutrition1_c, :default => false
+      t.integer :nutrition1_d, :default => 0
+      t.boolean :nutrition2_c, :default => false
+      t.integer :nutrition2_d, :default => 0
+      t.boolean :nutrition3_c, :default => false
+      t.integer :nutrition3_d, :default => 0
+      t.boolean :nutrition4_c, :default => false
+      t.integer :nutrition4_d, :default => 0
+      t.boolean :nutrition5_c, :default => false
+      t.integer :nutrition5_d, :default => 0
+      t.float :labor1_c, :default => 0
+      t.integer :labor1_d, :default => 0
+      t.float :labor2_c, :default => 0
+      t.integer :labor2_d, :default => 0
+      t.float :labor3_c, :default => 0
+      t.integer :labor3_d, :default => 0
+      t.float :labor4_c, :default => 0
+      t.integer :labor4_d, :default => 0
+      t.boolean :labor5_c, :default => false
+      t.integer :labor5_d, :default => 0
+      t.boolean :labor6_c, :default => false
+      t.integer :labor6_d, :default => 0
+      t.boolean :labor7_c, :default => false
+      t.integer :labor7_d, :default => 0
+      t.boolean :community1_c, :default => false
+      t.integer :community1_d, :default => 0
+      t.boolean :community2_c, :default => false
+      t.integer :community2_d, :default => 0
+      t.boolean :community3_c, :default => false
+      t.integer :community3_d, :default => 0
+      t.boolean :community4_c, :default => false
+      t.integer :community4_d, :default => 0
+      t.float :source1_c, :default => 0
+      t.integer :source1_d, :default => 0
+      t.boolean :conserv1_c, :default => false
+      t.integer :conserv1_d, :default => 0
+      t.boolean :conserv2_c, :default => false
+      t.integer :conserv2_d, :default => 0
+      t.boolean :conserv3_c, :default => false
+      t.integer :conserv3_d, :default => 0
+      t.boolean :conserv4_c, :default => false
+      t.integer :conserv4_d, :default => 0
+      t.boolean :conserv5_c, :default => false
+      t.integer :conserv5_d, :default => 0
+      t.boolean :conserv6_c, :default => false
+      t.integer :conserv6_d, :default => 0
+      t.boolean :conserv7_c, :default => false
+      t.integer :conserv7_d, :default => 0
+      t.boolean :conserv8_c, :default => false
+      t.integer :conserv8_d, :default => 0
+      t.float :conserv9_c, :default => 0
+      t.integer :conserv9_d, :default => 0
+      t.boolean :conserv10_c, :default => false
+      t.integer :conserv10_d, :default => 0
+      t.boolean :conserv11_c, :default => false
+      t.integer :conserv11_d, :default => 0
+      t.boolean :conserv12_c, :default => false
+      t.integer :conserv12_d, :default => 0
+      t.boolean :conserv13_c, :default => false
+      t.integer :conserv13_d, :default => 0
+      t.boolean :conserv14_c, :default => false
+      t.integer :conserv14_d, :default => 0
       t.string :optn1_c
-      t.integer :optn1_d
+      t.integer :optn1_d, :default => 0
       t.string :optn2_c
-      t.integer :optn2_d
+      t.integer :optn2_d, :default => 0
       t.string :optn3_c
-      t.integer :optn3_d
+      t.integer :optn3_d, :default => 0
       t.string :optn4_c
-      t.integer :optn4_d
+      t.integer :optn4_d, :default => 0
       t.string :optn5_c
-      t.integer :optn5_d
-      t.float :healthy_dining_points
-      t.float :healthy_work_points
-      t.float :community_benefits_points
-      t.float :food_sourcing_points
-      t.float :conversation_points
-      t.float :total_points
+      t.integer :optn5_d, :default => 0
+      t.float :healthy_dining_points, :default => 0
+      t.float :healthy_work_points, :default => 0
+      t.float :community_benefits_points, :default => 0
+      t.float :food_sourcing_points, :default => 0
+      t.float :conversation_points, :default => 0
+      t.float :total_points, :default => 0
 
       t.timestamps
     end
